612a227e9fd764ee6733915e473deb3f14ddc246
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2022-11-08 17:37:22 +0300

    DI for single container

---
+++ dependency-injection/single_container/__main__.py
@@ -0,0 +1,24 @@
+
+
+import sys
+from dependency_injector.wiring import Provide, inject
+from .containers import Container
+
+from .services import UserService
+
+
+@inject
+def main(email: str, user_service: UserService = Provide[Container.user_service]) -> None:
+    print('test')
+    user = user_service.get_user(email)
+    print(user)
+
+
+print(__name__)
+
+if __name__ == "__main__":
+    container = Container()
+    container.init_resources()
+    container.wire(modules=[__name__])
+
+    main(*sys.argv[1:])
---
+++ dependency-injection/tmp/email_reader.py
@@ -0,0 +1,13 @@
+
+
+
+class EmailReader(object):
+
+    def __init__(self,client):
+        try:
+            self._client = client
+        except Exception as e:
+            raise e
+
+    def read(self):
+        print('email reader read')

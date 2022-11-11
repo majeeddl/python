ffd83a606197227863a116512e4bb386a10d6d65
Majeed Dourandeesh <majeed.dourandeesh@profen.com>
2022-11-11 17:37:58 +0300

    working on flask app for dependency injection

--- dependency-injection/flask/containers.py
+++ dependency-injection/flask/containers.py
@@ -5,8 +5,22 @@ from dependency_injector import containers, providers
 from github import Gitghub
 
 
-from . import services
+import services
+
 
 class Container(containers.DeclarativeContainer):
 
     wiring_config = containers.WiringConfiguration(modules=[".views"])
+
+    config = providers.Configuration(yaml_files=["config.yml"])
+
+    github_client = providers.Factory(
+        Gitghub,
+        login_or_token=config.github.auth_token,
+        timeout=config.github.request_timeout
+        )
+        
+    search_service = providers.Factory(
+        services.SearchService,
+        github_client=github_client,
+    )
